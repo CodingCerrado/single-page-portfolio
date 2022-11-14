fa7561244def7e7f8b8cc1dc2f45919f166e9772
Joao Henrique <jhrodriguesit@gmail.com>
2022-11-14 23:12:16 +1000

    improve page fetching

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-async function getContent(paginaId, callback) {
-    var paginas = {};
+var paginas = {};
 
-    await fetch("paginas/resume/resume.html")
-      .then((res) => res.text())
-      .then((data) => paginas.resume = data);
-    await fetch("paginas/home/home.html")
-      .then((res) => res.text())
-      .then((data) => paginas.home = data);
-    await fetch("paginas/projects/projects.html")
-      .then((res) => res.text())
-      .then((data) => paginas.projects = data);
-    await fetch("paginas/contact/contact.html")
-      .then((res) => res.text())
-      .then((data) => paginas.contact = data);
+async function fetchPaginas() {
+  const contentDiv = document.getElementById("app");
+  contentDiv.innerHTML = "Loading page...";
 
-  callback(paginas[paginaId]);
+  await fetch("paginas/resume/resume.html")
+    .then((res) => res.text())
+    .then((data) => (paginas.resume = data));
+  await fetch("paginas/home/home.html")
+    .then((res) => res.text())
+    .then((data) => (paginas.home = data));
+  await fetch("paginas/projects/projects.html")
+    .then((res) => res.text())
+    .then((data) => (paginas.projects = data));
+  await fetch("paginas/contact/contact.html")
+    .then((res) => res.text())
+    .then((data) => (paginas.contact = data));
 }
 
-function carregarConteudo() {
-  var contentDiv = document.getElementById("app"),
-    paginaId = location.hash.substring(1);
+fetchPaginas().then(() => {
+  function getContent(paginaId, callback) {
+    callback(paginas[paginaId]);
+  }
 
-  getContent(paginaId, function (content) {
-    contentDiv.innerHTML = content;
-  });
+  function carregarConteudo() {
+    const contentDiv = document.getElementById("app"),
+      paginaId = location.hash.substring(1);
 
-}
+    getContent(paginaId, function (conteudoDaPagina) {
+      contentDiv.innerHTML = conteudoDaPagina;
+    });
+  }
 
-if (!location.hash) {
-  location.hash = "#home";
-}
+  if (!location.hash) {
+    location.hash = "#home";
+  }
 
-carregarConteudo();
+  carregarConteudo();
 
-window.addEventListener("hashchange", carregarConteudo);
+  window.addEventListener("hashchange", carregarConteudo);
+});
